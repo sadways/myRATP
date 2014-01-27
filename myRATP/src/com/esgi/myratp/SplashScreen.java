06ffebd2a245bd7510a64988db725df5dcf09a7d
Benjamin GRAUX <benjamin.graux@gmail.com>
2014-01-27 22:54:26 +0100

    splashscreen

---
+++ myRATP/src/com/esgi/myratp/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.esgi.myratp;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class SplashScreen extends Activity {
+
+
+	// Splash screen timer
+    private static int SPLASH_TIME_OUT = 5000;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
